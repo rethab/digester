1497f842a9242130caa74cdba746df1e6b5c34bb
Reto Habluetzel <rethab@protonmail.ch>
2020-01-07 12:47:30 +0100

    frontend: add prerendering

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: ['/']
+      })
+    ]
+  }
 }
