3c820669c55b112305fa67ec065ec7d893e63f54
Reto Habluetzel <rethab@protonmail.ch>
2020-01-07 18:10:16 +0100

    frontend/build: reduce bundle size by removing moment locales

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
 
 module.exports = {
   "transpileDependencies": [
@@ -10,7 +12,12 @@ module.exports = {
       new PrerenderSPAPlugin({
         staticDir: path.join(__dirname, 'dist'),
         routes: ['/']
-      })
+      }),
+      // ignore locales from moment to reduce size
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+      // enable for bundle size analysis
+      // new BundleAnalyzerPlugin(),
     ]
   }
 }
