fa57f4f2fb2a2e39c9a28c28789632b89c253732
Reto Habluetzel <rethab@protonmail.ch>
2020-01-08 12:47:35 +0100

    frontend/login: show loading indicator on github button

--- frontend/src/components/auth/GithubLoginBtn.vue
+++ frontend/src/components/auth/GithubLoginBtn.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn dark @click="authenticate('github')">
+  <v-btn dark :loading="loading" @click="authenticate('github')">
     Github
     <v-icon small class="pl-1">{{ githubIcon }}</v-icon>
   </v-btn>
@@ -10,24 +10,32 @@ import { mdiGithubCircle } from "@mdi/js";
 export default {
   data() {
     return {
+      loading: false,
       githubIcon: mdiGithubCircle
     };
   },
   methods: {
     authenticate(provider) {
+      this.loading = true;
       this.$store
         .dispatch("authenticate", {
           vueAuth: this.$auth,
           provider: provider
         })
         .then(resp => {
+          this.loading = false;
           if (resp.data.first_login) {
             this.$router.push({ name: "home", query: { firstLogin: true } });
           } else {
             this.$router.push({ name: "subscriptions" });
           }
         })
-        .catch(() => {});
+        .catch(err => {
+          if (!err.response) {
+            this.$store.dispatch("setOffline");
+          }
+          this.loading = false;
+        });
     }
   }
 };
