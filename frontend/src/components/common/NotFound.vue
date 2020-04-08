c80dd4494e88919189dcc157c0ccf35ff59158fd
Reto Hablützel <rethab@protonmail.ch>
2020-04-08 20:46:54 +0200

    frontend/list: show not found page for list that cannot be found

---
+++ frontend/src/components/common/NotFound.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-card class="mx-auto" width="500px" flat>
+      <v-card-title>This {{thing}} does not exist..</v-card-title>
+      <v-card-subtitle>..but we can help you</v-card-subtitle>
+      <v-card-text>
+        <ul style="list-style-type: none" class="pl-0">
+          <li>
+            Back to the
+            <router-link :to="link">Overview</router-link>
+          </li>
+          <li v-if="isAuthenticated">
+            Open
+            <router-link to="/cockpit">Cockpit</router-link>
+          </li>
+          <li>
+            Visit
+            <router-link to="/">digester.app</router-link>
+          </li>
+        </ul>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    thing: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  margin-top: 0.5em;
+}
+</style>
--- frontend/src/views/ShowList.vue
+++ frontend/src/views/ShowList.vue
@@ -1,12 +1,17 @@
 <template>
-  <ShowList v-if="list !== null" v-model="list" :dense="false" :checkSubscription="true" />
+  <div>
+    <ShowList v-if="list !== null" v-model="list" :dense="false" :checkSubscription="true" />
+    <NotFound v-else thing="List" link="/lists" />
+  </div>
 </template>
 
 <script>
+import NotFound from "@/components/common/NotFound.vue";
 import ShowList from "@/components/lists/ShowList.vue";
 import Vuex from "@/store/index.js";
 export default {
   components: {
+    NotFound,
     ShowList
   },
   data() {
@@ -15,18 +20,20 @@ export default {
     };
   },
   beforeRouteEnter(to, from, next) {
-    Vuex.dispatch("loadList", to.params.id).then(list => {
-      next(vm => vm.setList(list));
-    });
-    // todo error handling
+    Vuex.dispatch("loadList", to.params.id)
+      .then(list => {
+        next(vm => vm.setList(list));
+      })
+      .catch(() => next());
   },
   beforeRouteUpdate(to, from, next) {
     this.list = null;
-    Vuex.dispatch("loadList", to.params.id).then(list => {
-      this.setList(list);
-      next();
-    });
-    // todo error handling
+    Vuex.dispatch("loadList", to.params.id)
+      .then(list => {
+        this.setList(list);
+        next();
+      })
+      .catch(() => next());
   },
   methods: {
     setList(list) {
