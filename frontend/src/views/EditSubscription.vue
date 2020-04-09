759753624fbb8fa7851417942640683a36b6f853
Reto Hablützel <rethab@protonmail.ch>
2020-04-09 20:17:46 +0200

    frontend/subs: show not found page when editing subscriptions

--- frontend/src/views/EditSubscription.vue
+++ frontend/src/views/EditSubscription.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <EditSubscription v-if="subscription !== null" v-model="subscription" />
+    <NotFound v-else thing="Subscription" link="/subs" />
   </div>
 </template>
 
 <script>
+import NotFound from "@/components/common/NotFound.vue";
 import EditSubscription from "@/components/subs/EditSubscription.vue";
 import Vuex from "@/store/index.js";
 export default {
   components: {
+    NotFound,
     EditSubscription
   },
   data() {
@@ -17,18 +20,20 @@ export default {
     };
   },
   beforeRouteEnter(to, from, next) {
-    Vuex.dispatch("loadSubscription", to.params.id).then(subscription => {
-      next(vm => vm.setSubscription(subscription));
-    });
-    // todo error handling
+    Vuex.dispatch("loadSubscription", to.params.id)
+      .then(subscription => {
+        next(vm => vm.setSubscription(subscription));
+      })
+      .catch(() => next());
   },
   beforeRouteUpdate(to, from, next) {
     this.subscription = null;
-    Vuex.dispatch("loadSubscription", to.params.id).then(subscription => {
-      this.setSubscription(subscription);
-      next();
-    });
-    // todo error handling
+    Vuex.dispatch("loadSubscription", to.params.id)
+      .then(subscription => {
+        this.setSubscription(subscription);
+        next();
+      })
+      .catch(() => next());
   },
   methods: {
     setSubscription(subscription) {
