f4337e8dccdbbe9b6a947a82132c5a7cef478868
Reto Hablützel <rethab@protonmail.ch>
2020-05-03 16:50:39 +0200

    frontend: lighter footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-footer app absolute>
-    <router-link to="/privacy" style="text-decoration: none; color: inherit;">Privacy</router-link>
-    <v-spacer></v-spacer>
-    <v-btn href="https://twitter.com/digesterapp" target="_blank" icon>
-      <v-icon>{{twitterIcon}}</v-icon>
-    </v-btn>
-    <v-btn href="https://linkedin.com/company/digester" target="_blank" icon>
-      <v-icon>{{linkedinIcon}}</v-icon>
-    </v-btn>
-    <v-btn href="https://www.youtube.com/channel/UCWDpenaoBUsi0TmFiOK611A" target="_blank" icon>
-      <v-icon>{{youtubeIcon}}</v-icon>
-    </v-btn>
+  <v-footer app absolute class="grey--text">
+    <v-container class="pa-0">
+      <v-row class="ma-0" align="center">
+        <v-col class="pa-0 overline font-weight-thin">
+          <router-link to="/privacy" style="text-decoration: none; color: grey;">Privacy</router-link>
+          <span class="mx-1">|</span>
+          <router-link to="/terms" style="text-decoration: none; color: grey;">Terms</router-link>
+        </v-col>
+        <v-col class="text-center pa-0" v-if="$vuetify.breakpoint.smAndUp">Digester &copy; 2020</v-col>
+        <v-col class="text-right pa-0">
+          <v-btn
+            v-for="(social, idx) in socialLinks"
+            :key="idx"
+            :href="social.url"
+            icon
+            color="grey"
+            class="ml-2 text--lighten-1"
+            target="_blank"
+            small
+          >
+            <v-icon>{{social.icon}}</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
@@ -19,9 +32,20 @@ import { mdiTwitter, mdiYoutube, mdiLinkedin } from "@mdi/js";
 export default {
   data() {
     return {
-      twitterIcon: mdiTwitter,
-      youtubeIcon: mdiYoutube,
-      linkedinIcon: mdiLinkedin
+      socialLinks: [
+        {
+          url: "https://twitter.com/digesterapp",
+          icon: mdiTwitter
+        },
+        {
+          url: "https://linkedin.com/company/digester",
+          icon: mdiLinkedin
+        },
+        {
+          url: "https://www.youtube.com/channel/UCWDpenaoBUsi0TmFiOK611A",
+          icon: mdiYoutube
+        }
+      ]
     };
   }
 };
