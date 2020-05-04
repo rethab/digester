bfd8250af6fb7545ea39383406239cf511b93b71
Reto Hablützel <rethab@protonmail.ch>
2020-05-04 13:51:10 +0200

    frontend/navigation: move list and settings to advanced menu

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -29,15 +29,6 @@
         <v-list-item-title>Subscriptions</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item v-if="isAuthenticated" to="/lists">
-      <v-list-item-icon>
-        <v-icon>{{ updatesIcon }}</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>List Management</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
     <v-list-item v-if="isAuthenticated" to="/updates">
       <v-list-item-icon>
         <v-icon>{{ updatesIcon }}</v-icon>
@@ -47,14 +38,27 @@
       </v-list-item-content>
     </v-list-item>
     <v-divider></v-divider>
-    <v-list-item v-if="isAuthenticated" to="/settings">
-      <v-list-item-icon>
-        <v-icon>{{ settingsIcon }}</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>Settings</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+    <v-list-group no-action sub-group>
+      <template v-slot:activator>
+        <v-list-item-title>Advanced</v-list-item-title>
+      </template>
+      <v-list-item v-if="isAuthenticated" to="/lists">
+        <v-list-item-content>
+          <v-list-item-title>My Lists</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-icon>
+          <v-icon>{{ updatesIcon }}</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+      <v-list-item v-if="isAuthenticated" to="/settings">
+        <v-list-item-content>
+          <v-list-item-title>Settings</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-icon>
+          <v-icon>{{ settingsIcon }}</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list-group>
   </div>
 </template>
 
