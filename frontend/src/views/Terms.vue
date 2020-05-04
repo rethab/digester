aebb99f5ff4c53ea751fbc27f37ada026fa2e111
Reto Hablützel <rethab@protonmail.ch>
2020-05-04 08:31:05 +0200

    frontend/terms: improve wording, spelling

--- frontend/src/views/Terms.vue
+++ frontend/src/views/Terms.vue
@@ -29,11 +29,12 @@ export default {
         {
           title: "Limitations of Liability",
           content:
-            "Digester is not liable for any actions you may take based on content on our website." +
+            "Digester is not liable for any actions you may take based on content on our website or distributed via our services." +
             "<br />" +
-            "We have no liability for any loss or damage of any sort caused by malfunctioning of Digester. " +
-            "In particular, in case a digest (weekly or daily update) is (a) not sent out, (b) sent out with wrong contents, (c) " +
-            "sent out with incomplete content, or (d) sent out at the wrong time."
+            "<br />" +
+            "Digester has no liability for any loss or damage of any sort caused by malfunctioning of Digester. " +
+            "This includes cases where a digest (weekly or daily update) is not sent out, sent out with wrong contents, " +
+            "sent out with incomplete content, or sent out at the wrong time."
         },
         {
           title: "Your Content in our Service",
@@ -41,44 +42,46 @@ export default {
             "Some of our services allow you to create content. When you create content on our website, you give Digester a " +
             "worldwide license to use, host, store, reproduce, modify, create derivative work (eg. translations), communicte, publish and distribute that content." +
             "<br />" +
-            "The rights you grant are used to operate our services and the license does not expire when terminating the account."
+            "<br />" +
+            "The rights you grant are used to operate our services and the license does not expire when terminating your account."
         },
         {
           title: "Removal of Links from our Website",
           content:
-            "If you find any link on our Web site or any linked web site objectionable for any reason, you may contact" +
-            "us about this. We will consider requests to remove links but will have no obligation to do so or to respond" +
+            "If you find any link on our Web site or any linked web site objectionable for any reason, you may contact " +
+            "us about this. Digester will consider requests to remove links but will have no obligation to do so or to respond " +
             "directly to you." +
             "<br />" +
-            "Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness" +
-            "or accuracy; nor do we commit to ensuring that the website remains available or that the material on the" +
+            "<br />" +
+            "Whilst Digester endeavours to ensure that the information on this website is correct, Digester does not warrant its completeness " +
+            "or accuracy; nor does Digester commit to ensuring that the website remains available or that the material on the " +
             "website is kept up to date."
         },
         {
           title: "Suspension and Termination of Service",
           content:
             "Digester may limit, suspend, or stop proving the services to you without notice if you fail to " +
-            "comply (or are suspected to) with these terms, or if you use" +
+            "comply (or are suspected to) with these terms, or if you use " +
             "the service in a way that causes legal liability or is disruptive to Digester or any other user."
         },
         {
           title: "Cookies",
           content:
-            "We employ the use of cookies. By using Digester's website you consent to the use of cookies in accordance with Digester's privacy policy."
+            "Digester employs the use of cookies. By using Digester's website you consent to the use of cookies in accordance with Digester's privacy policy."
         },
         {
           title: "Content Liability",
           content:
-            "We shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify" +
-            "and defend us against all claims arising out of or based upon your Website. No link(s) may appear on any" +
-            "page on your Web site or within any context containing content or materials that may be interpreted as" +
-            "libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or" +
+            "Digester shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify " +
+            "and defend us against all claims arising out of or based upon your Website. No link(s) may appear on any " +
+            "page on your Web site or within any context containing content or materials that may be interpreted as " +
+            "libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or " +
             "other violation of, any third party rights."
         },
         {
           title: "Disclaimer",
           content:
-            "To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website (including, without limitation, any warranties implied by law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill). Nothing in this disclaimer will:" +
+            "To the maximum extent permitted by applicable law, digester excludes all representations, warranties and conditions relating to our website and the use of this website (including, without limitation, any warranties implied by law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill). Nothing in this disclaimer will:" +
             "<ul>" +
             "<li>limit or exclude our or your liability for death or personal injury resulting from negligence;</li>" +
             "<li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>" +
@@ -94,7 +97,7 @@ export default {
           title: "Entire Agreement",
           content:
             "These terms constitute the entire agreement between you and Digester with respect to the subject matter of these Terms, and " +
-            "supersede and replace any other prio or contemporaneous agreements, or terms and conditions applicable to the subject matter of these Terms." +
+            "supersede and replace any other prior or contemporaneous agreements, or terms and conditions applicable to the subject matter of these Terms. " +
             "These terms create no third party beneficiary rights."
         }
       ]
